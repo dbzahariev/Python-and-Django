f8f445fdfcd8e19c1a301030a600e98752c2877c
d.zahariev <d.zahariev@devrealm.bg>
2019-05-18 21:28:38 +0300

    Add new files

--- Django/Exam_Project/my_project/accounts/urls.py
+++ Django/Exam_Project/my_project/accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 from . import views
 
 urlpatterns = [
-    path('profile/', views),
+    path('profile/', views.redirect_to_user, name='user-redirect'),
+    re_path('^profile/(?P<pk>\d+)/$', views.UserProfileDetails.as_view(), name='details'),
     path('', include('django.contrib.auth.urls')),
 ]
--- Django/Exam_Project/my_project/accounts/views.py
+++ Django/Exam_Project/my_project/accounts/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.views import generic
+
+
+def redirect_to_user(request):
+    url = f"/accounts/profile/{request.user.id}/"
+    return HttpResponseRedirect(redirect_to=url)
+
+
+class UserProfileDetails(generic.DetailView):
+    model = User
+    template_name = 'user_profile.html'
+    context_object_name = 'user'
