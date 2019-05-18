5067e000cd39e289a73c663361610fd98b5d24b9
d.zahariev <d.zahariev@devrealm.bg>
2019-05-18 12:01:10 +0300

    Add new files

---
+++ Django/Exam_Project/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\work\projects\python-and-django\django\exam_project\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
