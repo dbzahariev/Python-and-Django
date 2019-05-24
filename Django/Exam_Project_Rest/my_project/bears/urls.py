c3f3c8aa827c282e34c11feba66d43ea8d442142
d.zahariev <d.zahariev@devrealm.bg>
2019-05-24 20:09:52 +0300

    Upgrade details for bears.

--- Django/Exam_Project_Rest/my_project/bears/urls.py
+++ Django/Exam_Project_Rest/my_project/bears/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.BearList.as_view(), name='ListBears'),
-    re_path('^details/(?P<pk>\d+)/$', views.BearDetails.as_view(), name='bear-detail')
+    re_path('^details/(?P<pk>\d+)/$', views.BearDetails.as_view(), name='bear-detail'),
+    re_path('mine/', views.UserBears.as_view(), name='user-bears')
 ]
--- Django/Exam_Project_Rest/my_project/bears/views.py
+++ Django/Exam_Project_Rest/my_project/bears/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
-from .models import Bear
+from .models import Bear, ProfileUser
 
 
 # Create your views here.
@@ -12,7 +13,24 @@ class BearList(generic.ListView):
     context_object_name = 'bears'
 
 
-class BearDetails(generic.DetailView):
+class BearDetails(LoginRequiredMixin, generic.DetailView):
+    login_url = '/accounts/login/'
     model = Bear
     template_name = 'bear_detail.html'
     context_object_name = 'bear'
+
+
+class UserBears(LoginRequiredMixin, generic.ListView):
+    login_url = '/accounts/login/'
+    model = Bear
+    template_name = 'bear_list.html'
+    context_object_name = 'bears'
+
+    def get_queryset(self):
+        user_id = int(self.request.user.id)
+        try:
+            author = ProfileUser.objects.all().filter(user__pk=user_id)[0]
+            bears = Bear.objects.all().filter(author=author.pk)
+            return bears
+        except:
+            return []
